b094b54d73fa3df3bceca2af2fb1d5c7be8b57a9
Mahdi-Suleiman <mahdi.suleimann@gmail.com>
2022-02-18 14:47:05 +0200

    third commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,10 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    public function tables()
+    {
+        return $this->belongsToMany(Table::class);
+    }
     /**
      * The attributes that are mass assignable.
      *
@@ -21,6 +25,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role'
     ];
 
     /**
