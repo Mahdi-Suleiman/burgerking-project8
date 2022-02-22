f5719717831f809f076119f7f1babf4c22bc90b7
Mahdi-Suleiman <mahdi.suleimann@gmail.com>
2022-02-22 11:07:20 +0200

    admin layout edit

--- resources/views/layouts/admin/index.blade.php
+++ resources/views/layouts/admin/index.blade.php
@@ -5,57 +5,57 @@
 
     <div class="row g-4">
         <div class="col-sm-6 col-xl-3">
-            <a class="nav-item nav-link">
 
             <div class="bg-light rounded d-flex align-items-center justify-content-between p-4">
                 {{-- <i class="fa fa-chart-line fa-3x text-primary"></i> --}}
                 {{-- <i class="fa fa-tachometer-alt me-2 fa-3x text-primary"></i> --}}
                 {{-- <i class="fa fa-ticket fa-3x text-primary"></i> --}}
                 {{-- <i class="fas fa-ticket-alt me-2 fa-3x text-primary"></i> --}}
+                <a class="nav-item nav-link">
                 <i class="fas fa-calendar-check me-2 fa-3x text-primary"></i>
+            </a>
                 <div class="ms-3">
                     <p class="mb-2">Total Reservations</p>
                     <h6 class="mb-0">{{$totalCount}}</h6>
                 </div>
             </div>
-        </a>
         </div>
         <div class="col-sm-6 col-xl-3">
-            <a href="{{route('admin.users')}}" class="nav-item nav-link">
 
             <div class="bg-light rounded d-flex align-items-center justify-content-between p-4">
+                <a href="{{route('admin.users')}}" class="nav-item nav-link">
                 {{-- <i class="fa fa-chart-bar fa-3x text-primary"></i> --}}
                 <i class="fas fa-users me-2 fa-3x text-primary"></i>
+            </a>
                 <div class="ms-3">
                     <p class="mb-2">Registered Users</p>
                     <h6 class="mb-0">{{$allUsers}}</h6>
                 </div>
             </div>
-            </a>
         </div>
         <div class="col-sm-6 col-xl-3">
-            <a href="{{route('admin.tables')}}" class="nav-item nav-link">
 
             <div class="bg-light rounded d-flex align-items-center justify-content-between p-4">
+                <a href="{{route('admin.tables')}}" class="nav-item nav-link">
                 <i class="fa fa-table me-2 fa-3x text-primary"></i>
+            </a>
                 <div class="ms-3">
                     <p class="mb-2">Total Tables</p>
                     <h6 class="mb-0">{{$allTables}}</h6>
                 </div>
             </div>
-            </a>
         </div>
         <div class="col-sm-6 col-xl-3">
-            <a href="{{route('admin.contacts')}}" class="nav-item nav-link ">
 
             <div class="bg-light rounded d-flex align-items-center justify-content-between p-4">
+                <a href="{{route('admin.contacts')}}" class="nav-item nav-link ">
                 <i class="fa fa-address-book me-2 fa-3x text-primary"></i>
                 <div class="ms-3">
+                </a>
                     <p class="mb-2">Total Contacts</p>
                     <h6 class="mb-0">{{$allContacts}}</h6>
                 </div>
             </div>
-        </a>
         </div>
     </div>
 </div>
